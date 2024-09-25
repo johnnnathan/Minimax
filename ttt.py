556e6c7522fddef93ca5719185050a3ac7a57b99
Dimi <johnnnathan@proton.me>
2024-09-25 14:58:44 +0200

    added input

---
+++ ttt.py
@@ -0,0 +1,84 @@
+player_symbol = "X"
+ai_symbol = "O"
+
+main_board = [["X",1,2],
+              [3,4,5],
+              [6,7,8]] 
+
+
+
+def print_board():
+    for row in main_board:
+        print(row)
+
+print_board()
+
+def finished_party(board, is_ai):
+    symbol = ai_symbol if is_ai else player_symbol
+
+    # Check rows
+    for row in board:
+        if all(cell == symbol for cell in row):
+            return True
+
+    # Check columns
+    for col in range(3):
+        if all(board[row][col] == symbol for row in range(3)):
+            return True
+
+    # Check diagonals
+    if (board[0][0] == symbol and board[1][1] == symbol and board[2][2] == symbol) or \
+       (board[0][2] == symbol and board[1][1] == symbol and board[2][0] == symbol):
+        return True
+    return False
+
+def finished(board):
+    return finished_party(board, False)  or finished_party(board, True)
+
+print(finished(main_board))
+
+def get_input():
+    while True:
+        move = input("What is your next move?")
+        try:
+            result = int(move)
+            if result < 9 and result > -1:
+                row = int(result / 3)
+                col = result % 3 
+                print(row)
+                print(col)
+                if main_board[row][col] != "X" and main_board[row][col] != "O":
+                    return result
+                else:
+                    print("Position already occupied")
+            else:
+                print("Value out of range")
+        except ValueError:
+            print("Invalid value, please try again")
+
+# def act_player():
+
+
+def act(who):
+    if who == player_symbol:
+        act_player()
+    else:
+        act_ai()
+
+
+def play(starter):
+    player1, player2 = "ERROR", "ERROR"
+    if starter == player_symbol:
+        player1 = player_symbol
+        player2 = ai_symbol
+    else:
+        player1 = ai_symbol
+        player2 = player_symbol
+
+    print("hello")
+        
+
+
+get_input()
+play("X")
+
